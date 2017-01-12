5a277d99d0129cbf4f4f3300dc249e90966ef7d3
Jordforb@gmail.com <Jordanforbes1@Jordans-MacBook-Pro-2.local>
2017-01-12 14:07:35 -0500

    analyzed the code

--- work.js
+++ work.js
@@ -90,39 +90,82 @@ window.addEventListener("keypress",function(event){
 
 //DYLAN'S CODE
 
-var header = document.getHelementByID("header")
+var header = document.getElementById("header")
+//the variable header tells the html to look inside the document
+//look for an element with the id 'header'
+var button = document.getElementById("login-button")
+//the variable button tells the html to look inside the document
+//look for an element with the id 'login-button'
 
-function checkPassword(){
-  var password = document.getElementByID("password").value
+//declare a header variable and a button variable
 
-  if (password =="12345678") {
-     return true
-  } else{
-    return false
+button.addEventListener("click", function() {
+  validateForm()
+})
+//listen inside the variable 'button' for the event 'click',
+//and when the click is heard use the function which calls
+//the other function 'validateForm()'
+
+function validateForm(){
+//declare the function 'validateForm'
+  if(checkPassword() == true && checkUsername() == true) {
+    //checks inside the function 'checkpassword' to see if the answer is true
+    //also checks the function 'checkUsername' to see if that answer is true
+    header.innerHTML = "<h1>You're now logged in!</h1>"
+    //if both are true then the javascript looks inside the header,
+    //inside the HTML text within the header, and tells the html that
+    //that text is now <h1>You're now logged in!</h1>
+  } else {
+    //if only one or neither are true, then...
+    alert("Incorrect!")
+    //send an alert announcing that it was incorrect
   }
 }
 
-function checkUsername(){
+function checkPassword() {
+//declare the function "checkPassword"
+  var password = document.getElementById("password").value
+  //within the function checkPassword, declare the variable "password"
+  //that variable tells the html to look inside the document,
+  //inside the document, the javascript then tells the html to look for the
+  //element with the id of "password" and to look inside it to find the value
+  if (password == "12345678") {
+    //if the value of the pasword is 12345678...
+     return true
+     //then say that it's true, if the value is not that...
+  } else {
+    //then say that it's false.
+    return false
+  }
+}
+//this "true" or "false" gets sent to the function "validateForm" to check the password
+
+function checkUsername() {
+//declare the function "checkUsername"
   var username = document.getElementById("username").value
+  //within the checkUsername function, decalre the variable "username"
+  //the variable "username" tells the HTML to look inside the document
+    //within the document, look for an element with the id of "username"
+      //within that element look for its value
   var characters = username.split("")
-  var presence = characters.map(function(element){
+  //within the checkUsername function, declare the variable "characters"
+  //the variable "characters" tells the HTML to look inside the "username" variable
+    //split the variable string up and add quotations. possibly. Ask Dylan what that does.
+  var presence = characters.map(function(element) {
+  //within the checkUsername function, declare the variable "presence"
+  //the variable "presence" tells the HTML to look inside the variable "characters"
+    //look inside its map? Ask Dylan.
     return Number.isInteger(parseInt(element))
   })
 
-  if (presence.indexOf(true) !== -1){
+  if (presence.indexOf(true) !== -1) {
     return true
-  }else{
+  } else {
     return false
   }
 }
 
-function validateForm(){
-  if(checkPassword()==true && checkUsername ==true){
-    header.innerHTML = "<h1>You're now logged in!</h1>"
-  }else{
-    alert("Incorrect!")
-  }
-}
+//periods add properties which tell the html to look inside something
 
 
 
